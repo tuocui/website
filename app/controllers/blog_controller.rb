6cfc8ecdf690c69bc06acf466a7369d8b6ac8201
Tony Cui <tony.cui@bazaarvoice.com>
2014-01-24 16:20:36 -0600

    header initial

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,5 @@
+class BlogController < ApplicationController
+  def index
+    render 'index'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Website::Application.routes.draw do
+
+  root :to => "blog#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
