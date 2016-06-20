37bd701cd204c3a825d35dc1be7b02c4a41d11b2
Tony Cui <tcui@fb.com>
2016-06-19 17:12:50 -0700

    different keyword

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,13 @@ Website::Application.routes.draw do
     get '/analytics' => :analytics
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create], :requirements => { :protocol => "https", :subdomain => "secure" }
 
   controller :sessions do
-    get '/login' => :new, :constraints => { :protocol => "https", :subdomain => "secure" }
-    get '/logout' => :destroy, :constraints => { :protocol => "https", :subdomain => "secure" }
+    get '/login' => :new, :requirements => { :protocol => "https", :subdomain => "secure" }
+    get '/logout' => :destroy, :requirements => { :protocol => "https", :subdomain => "secure" }
   end
-  resources :sessions, only: [:create], :constraints => { :protocol => "https", :subdomain => "secure" }
+  resources :sessions, only: [:create], :requirements => { :protocol => "https", :subdomain => "secure" }
 
   get "*path", :to => "pages#error_404"
 end
